7874e1c04c01f82785dac41ea083703dc0333722
Ambroz Bizjak <ambroz@soniox.com>
2022-06-21 06:05:33 -0700

    Move SimulatedCaptureDevice to capture_device.py.

--- soniox/capture_device.py
+++ soniox/capture_device.py
@@ -1,4 +1,6 @@
 import numpy
+import time
+import random
 from abc import ABC, abstractmethod
 
 
@@ -64,3 +66,59 @@ class MicrophoneCaptureDevice(AbstractCaptureDevice):
         samples = numpy.ascontiguousarray(samples, "<h")
         data = samples.tobytes()
         return data
+
+
+class SimulatedCaptureDevice(AbstractCaptureDevice):
+    """This class implements AbstractCaptureDevice by reading raw audio
+    samples from a file (encoding must be PCM 16-bit little endian 16 kHz)."""
+
+    def __init__(self, audio_file: str, start_random: bool = False) -> None:
+        # Read the audio file.
+        with open(audio_file, "rb") as fh:
+            self._audio = fh.read()
+
+        # Calculate the chunk size in bytes.
+        # Multiply by 2 because the files use 16-bit encoding.
+        self._chunk_size = 2 * PREFERRED_FRAME_SIZE
+
+        # Calculate the number of chunks in the audio.
+        num_chunks = len(self._audio) // self._chunk_size
+        if num_chunks == 0:
+            raise Exception(f"File {audio_file} does not have enough audio for one chunk.")
+
+        # Calculate the time delay between chunks to simulate real-time audio.
+        self._delay_sec = PREFERRED_FRAME_SIZE / SAMPLE_RATE
+
+        # Set the initial audio position.
+        if start_random:
+            self._position = random.randrange(num_chunks) * self._chunk_size
+        else:
+            self._position = 0
+
+        # Set the initial time.
+        self._time = time.monotonic()
+
+    def start(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    def read_audio(self) -> bytes:
+        # Delay to simulate real-time audio.
+        now = time.monotonic()
+        delay = self._time + self._delay_sec - now
+        if delay > 0.0:
+            time.sleep(delay)
+        self._time = time.monotonic()
+
+        # If there is not enough audio left for one chunk, reset the
+        # position to the beginning.
+        if len(self._audio) - self._position < self._chunk_size:
+            self._position = 0
+        # Get one chunk of audio.
+        audio_chunk = self._audio[self._position : self._position + self._chunk_size]
+        # Increment the audio position.
+        self._position += self._chunk_size
+        # Return the chunk.
+        return audio_chunk
