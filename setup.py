d6ff0ab043ada743855ea9d95465651109ef6ab3
Ambroz Bizjak <ambroz@soniox.com>
2022-06-21 08:16:47 -0700

    Update version to 1.2.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="soniox",
-    version="1.1.1",
+    version="1.2.0",
     author="Soniox Inc",
     author_email="support@soniox.com",
     description="Soniox speech recognition service client library",
